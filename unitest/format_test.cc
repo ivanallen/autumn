182703392f1dde0424d375fbc1c7749418bf6cd6
ivanallen <1007729991@qq.com>
2019-11-24 12:35:00 +0800

    format

--- unitest/format_test.cc
+++ unitest/format_test.cc
@@ -5,11 +5,24 @@ using namespace autumn;
 
 namespace {
 
+struct Foo {
+    Foo(int a, const std::string& b) : a(a), b(b) {}
+
+    int a;
+    std::string b;
+};
+
+std::ostream& operator<<(std::ostream& out, const Foo& foo) {
+    return out << '{' << foo.a << ',' << foo.b << '}';
+}
+
 TEST(Format, TestFormat) {
     EXPECT_EQ("my name is autumn", format("my name is {}", "autumn"));
     EXPECT_EQ("3 + 4 = 7", format("{} + {} = {}", 3, 4, 3 + 4));
-
-    EXPECT_EQ("error 3", format("error {}", 3, 4, 3 + 4));
+    EXPECT_EQ("3 + 4 = 7", format("{:a} + {:b} = {:c}", 3, 4, 3 + 4));
+    EXPECT_EQ("3 4", format("{} {}", 3, 4, 3 + 4));
+    EXPECT_EQ("{1,2,3}", format("{{},{},{}}", 1, 2, 3));
+    EXPECT_EQ("this is foo:{10,autumn}", format("this is foo:{}", Foo(10, "autumn")));
 }
 
 }
